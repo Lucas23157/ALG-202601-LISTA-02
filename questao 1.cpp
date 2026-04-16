3c8ccbf32a7f59e438c8f69c3a92d094945ceed3
Paulo Lucas Garçoni <lucasgarconi@gmail.com>
2026-04-16 09:54:48 -0300

    Add files via upload

---
+++ questao 1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int total_segundos;
+    int horas, minutos, segundos;
+
+    printf("Digite o tempo em segundos: ");
+    scanf("%d", &total_segundos);
+
+    horas = total_segundos / 3600;
+    total_segundos = total_segundos % 3600;
+
+    minutos = total_segundos / 60;
+    segundos = total_segundos % 60;
+
+    printf("Resultado: %d horas, %d minutos e %d segundos\n", horas, minutos, segundos);
+
+    return 0;
+}
